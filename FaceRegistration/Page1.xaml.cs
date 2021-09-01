6f5910d005e5be53b796def23f5e64662b91e7bb
Sergiy <67895331+Sergiy7-lab@users.noreply.github.com>
2021-09-01 17:00:05 +0300

    add
    add

---
+++ FaceRegistration/Page1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FaceRegistration
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+
+            image.Source = ImageSource.FromResource("FaceRegistration.assets.normal_u32.png");
+            face.Source = ImageSource.FromResource("FaceRegistration.assets.u73.png");
+            
+            }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void Button_Home(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+    }
+}
---
+++ FaceRegistration/Page2.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FaceRegistration
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+
+            
+            territory.Source = ImageSource.FromResource("FaceRegistration.assets.u258.png");
+            house.Source = ImageSource.FromResource("FaceRegistration.assets.u259.png");
+         
+
+            phone.Source = ImageSource.FromResource("FaceRegistration.assets.u260.png");
+            home.Source = ImageSource.FromResource("FaceRegistration.assets.u261.png");
+            settings.Source = ImageSource.FromResource("FaceRegistration.assets.u262.png");
+        }
+
+
+        private void phone_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+
+        private void settings_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+
+        private void home_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+
+
+
+        private void Territory_Tapped(object sender, EventArgs e)
+        {
+            territory.Source = ImageSource.FromResource("FaceRegistration.assets.u266.png");
+            territory_text.Text = "The door is open";
+
+        }
+
+        private void House_Tapped(object sender, EventArgs e)
+        {
+            house.Source = ImageSource.FromResource("FaceRegistration.assets.u266.png");
+            house_text.Text = "The door is open";
+        }
+    }
+}
